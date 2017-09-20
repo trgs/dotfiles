2a46c72099fd3b563ab99e882b4ae1de5825e12e
triggs <triggs@fileserver>
2017-09-20 23:09:09 +0200

    Tested release on local server

--- install.sh
+++ install.sh
@@ -21,11 +21,18 @@ backup ()
   fi
 }
 
-BACKUP="$HOME/.dotfiles_backup/$(date +%s)"
+BACKUP="$HOME/.dotfiles_backup/"
+
 if ! test -d $BACKUP
 then
   mkdir $BACKUP
 fi
+
+BACKUP=$BACKUP$(date +%s)
+mkdir $BACKUP
+
+echo -n "Make sure you have the following installed: python cmake vim-gtk3, press any key to continue..."
+read
 
 # do NOT use tailing /'s
 echo "[+] Moving old configuration files to $BACKUP"
@@ -35,14 +42,18 @@ backup "$HOME/.vimrc" $BACKUP
 
 echo -n "[+] Creating symlinks:"
 echo -n " bash"
-ln -s $PWD/.bashrc $HOME/.bashrc
+ln -s $PWD/bashrc $HOME/.bashrc
 
 echo -n " vim"
-ln -s $PWD/.vimrc $HOME/.vimrc
-ln -s $PWD/.vim $HOME/.vim
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/vim $HOME/.vim
 
 echo "[+] Installing VundleVim"
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+echo -n "[!] Press any key to start vim, use ':PluginInstall' and wait till all have been set up. Exit vim with ':q' to continue this install script..."
+read
+vim
 
 echo "[+] Compiling YouCompleteMe"
 $HOME/.vim/bundle/YouCompleteMe/install.py --all
